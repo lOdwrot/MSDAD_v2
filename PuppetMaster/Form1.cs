41e0efbdfbe6b67a0e5e439a7102bb2177b21e81
LucasOdwrot <rebelionx123@gmail.com>
2019-10-28 08:33:19 +0000

    Finished UI for PM

--- PuppetMaster/Form1.cs
+++ PuppetMaster/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,58 @@ using System.Windows.Forms;
 
 namespace PuppetMaster
 {
+    
     public partial class Form1 : Form
     {
+        private static string CLIENT_EXECUTABLE_PATH = "xxx";
+        private static string SERVER_EXECUTABLE_PATH = "xxx";
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInstantiateServer_Click(object sender, EventArgs e)
+        {
+            String sId = serverId.Text;
+            String sURL = serverURL.Text;
+            int sMaxFaults = Int32.Parse(serverMaxFaults.Text);
+            int sMinDelay = Int32.Parse(serverMinDelay.Text);
+            int sMaxDelay = Int32.Parse(serverMaxDelay.Text);
+
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "C:\\Windows\\System32\\fsutil.exe",
+                        Arguments = "behavior query SymlinkEvaluation",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    var line = process.StandardOutput.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+                process.WaitForExit();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+            }
         }
     }
 }
